50479ac07d961456066949f63da842685fedac55
Codegeek2004 <singhjanvi2021@gmail.com>
2023-04-30 14:03:05 +0530

    Create Valid_Anagrams.py

---
+++ Valid_Anagrams.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        ss=sorted(s)
+        ts=sorted(t)
+        if (ss==ts):
+            return True
+        else:
+            return False
